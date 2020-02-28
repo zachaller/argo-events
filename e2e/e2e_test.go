c9cd4436cc432f36bc55ba521dfd7deb7abb8f78
VaibhavPage <vaibhav.page@gmail.com>
2020-02-28 15:57:16 -0500

    feat: added e2e files

--- e2e/e2e_test.go
+++ e2e/e2e_test.go
@@ -1,24 +1,55 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
+	"testing"
+
 	"github.com/argoproj/argo-events/common"
 	gw_v1alpha1 "github.com/argoproj/argo-events/pkg/apis/gateway/v1alpha1"
 	sensorv1alpha1 "github.com/argoproj/argo-events/pkg/apis/sensor/v1alpha1"
+	sn_v1alpha1 "github.com/argoproj/argo-events/pkg/apis/sensor/v1alpha1"
 	esv1alpha1 "github.com/argoproj/argo-events/pkg/client/eventsources/clientset/versioned/typed/eventsources/v1alpha1"
 	gwv1alpha1 "github.com/argoproj/argo-events/pkg/client/gateway/clientset/versioned/typed/gateway/v1alpha1"
 	snv1alpha1 "github.com/argoproj/argo-events/pkg/client/sensor/clientset/versioned/typed/sensor/v1alpha1"
-	wfv1alpha1 "github.com/argoproj/argo/pkg/client/clientset/versioned/typed/workflow/v1alpha1"
+	wf_v1alpha1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 	"github.com/google/go-github/github"
 	"github.com/mitchellh/go-homedir"
-	occlient "github.com/openshift/client-go/route/clientset/versioned"
+	ocv1alpha1 "github.com/openshift/api/route/v1"
 	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/kubernetes"
-	"os"
-	"testing"
+	"k8s.io/client-go/kubernetes/scheme"
+)
+
+// NOTE: custom resources must be manually added here
+func init() {
+	if err := wf_v1alpha1.AddToScheme(scheme.Scheme); err != nil {
+		panic(err)
+	}
+	if err := sn_v1alpha1.AddToScheme(scheme.Scheme); err != nil {
+		panic(err)
+	}
+	if err := gw_v1alpha1.AddToScheme(scheme.Scheme); err != nil {
+		panic(err)
+	}
+	if err := gw_v1alpha1.AddToScheme(scheme.Scheme); err != nil {
+		panic(err)
+	}
+	if err := ocv1alpha1.AddToScheme(scheme.Scheme); err != nil {
+		panic(err)
+	}
+}
+
+var (
+	registry = runtime.NewEquivalentResourceRegistry()
 )
 
 const namespace = "argo-events"
@@ -38,20 +69,63 @@ type E2ETestSuite struct {
 	K8sClient         kubernetes.Interface
 	GatewayClient     gwv1alpha1.ArgoprojV1alpha1Interface
 	SensorClient      snv1alpha1.ArgoprojV1alpha1Interface
-	WorkflowClient    wfv1alpha1.ArgoprojV1alpha1Interface
 	GithubClient      *github.Client
 	EventSourceClient esv1alpha1.ArgoprojV1alpha1Interface
-	OCRouteClient     occlient.Interface
+	DynamicClient     dynamic.Interface
 	Branch            string
 	Logger            *logrus.Logger
 	ProjDir           string
+	DomainName        string
+}
+
+func decodeAndUnstructure(b []byte, gvr *metav1.GroupVersionResource) (*unstructured.Unstructured, error) {
+	gvk := registry.KindFor(schema.GroupVersionResource{
+		Group:    gvr.Group,
+		Version:  gvr.Version,
+		Resource: gvr.Resource,
+	}, "")
+
+	obj, _, err := scheme.Codecs.UniversalDeserializer().Decode(b, &gvk, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	uObj, err := runtime.DefaultUnstructuredConverter.ToUnstructured(obj)
+	if err != nil {
+		return nil, err
+	}
+	return &unstructured.Unstructured{Object: uObj}, nil
+}
+
+func encodeWorkflowFromUnstructured(obj *unstructured.Unstructured) (*wf_v1alpha1.Workflow, error) {
+	body, err := obj.MarshalJSON()
+	if err != nil {
+		return nil, err
+	}
+	var wf *wf_v1alpha1.Workflow
+	if err := json.Unmarshal(body, &wf); err != nil {
+		return nil, err
+	}
+	return wf, nil
+}
+
+func wfGVR() schema.GroupVersionResource {
+	return schema.GroupVersionResource{
+		Group:    "argoproj.io",
+		Version:  "v1alpha1",
+		Resource: "workflows",
+	}
+}
+
+func routeGVR() schema.GroupVersionResource {
+	return schema.GroupVersionResource{
+		Group:    ocv1alpha1.GroupName,
+		Version:  ocv1alpha1.GroupVersion.Version,
+		Resource: "routes",
+	}
 }
 
 func (test *E2ETestSuite) SetupTest() {
-	os.Setenv(common.EnvVarKubeConfig, "/Users/vpage/.kube/config")
-	os.Setenv("KUBERNETES_SERVICE_HOST", "https://dev-k8s-ewd.dev.blackrock.com")
-	os.Setenv("KUBERNETES_SERVICE_PORT", "8443")
-
 	// kubernetes configuration
 	kubeConfig, _ := os.LookupEnv(common.EnvVarKubeConfig)
 	restConfig, err := common.GetClientConfig(kubeConfig)
@@ -59,13 +133,18 @@ func (test *E2ETestSuite) SetupTest() {
 		panic(err)
 	}
 
+	domainName, ok := os.LookupEnv("DOMAIN_NAME")
+	if !ok {
+		panic("no domain name")
+	}
+
 	test.K8sClient = kubernetes.NewForConfigOrDie(restConfig)
 	test.GatewayClient = gwv1alpha1.NewForConfigOrDie(restConfig)
 	test.SensorClient = snv1alpha1.NewForConfigOrDie(restConfig)
-	test.WorkflowClient = wfv1alpha1.NewForConfigOrDie(restConfig)
 	test.EventSourceClient = esv1alpha1.NewForConfigOrDie(restConfig)
-	test.OCRouteClient = occlient.NewForConfigOrDie(restConfig)
+	test.DynamicClient = dynamic.NewForConfigOrDie(restConfig)
 	test.GithubClient = github.NewClient(nil)
+	test.DomainName = domainName
 	test.Branch = "master"
 	dir, _ := homedir.Dir()
 	test.ProjDir = dir + "/go/src/github.com/argoproj/argo-events"
@@ -126,7 +205,7 @@ func (test *E2ETestSuite) WaitForSensorReadyState(name string) error {
 	return nil
 }
 
-func (test *E2ETestSuite) DeleteAll(t *testing.T) {
+func (test *E2ETestSuite) DeleteAllResources(t *testing.T) {
 	err := test.EventSourceClient.EventSources(namespace).DeleteCollection(&metav1.DeleteOptions{}, metav1.ListOptions{})
 	require.Nil(t, err)
 
@@ -144,7 +223,9 @@ func (test *E2ETestSuite) TestAll() {
 
 	test.WebhookSetupTest(test.T())
 
-	test.DeleteAll(test.T())
+	test.WebhookResourceTest(test.T(), "webhook-gateway", "webhook-sensor")
+
+	test.DeleteAllResources(test.T())
 
 	err = test.DeleteControllers()
 	require.Nil(test.T(), err)
--- e2e/webhook_test.go
+++ e2e/webhook_test.go
@@ -1,18 +1,22 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
+	"net/http"
+	"strings"
 	"testing"
+	"time"
 
 	eventSourceV1Alpha1 "github.com/argoproj/argo-events/pkg/apis/eventsources/v1alpha1"
 	gw_v1alpha1 "github.com/argoproj/argo-events/pkg/apis/gateway/v1alpha1"
 	sensorv1alpha1 "github.com/argoproj/argo-events/pkg/apis/sensor/v1alpha1"
 	"github.com/ghodss/yaml"
-	"github.com/kubernetes/apimachinery/pkg/util/intstr"
 	ocv1 "github.com/openshift/api/route/v1"
 	"github.com/stretchr/testify/require"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
 )
 
 func (test *E2ETestSuite) CreateWebhookEventSource() (*eventSourceV1Alpha1.EventSource, error) {
@@ -80,28 +84,73 @@ func (test *E2ETestSuite) WebhookResourceTest(t *testing.T, gatewayName, sensorN
 	require.Nil(t, err)
 
 	require.Equal(t, gw_v1alpha1.NodePhaseRunning, gateway.Status.Phase)
-	require.Equal(t, 1, len(gateway.Status.Nodes))
 
 	sensor, err := test.SensorClient.Sensors(namespace).Get(sensorName, metav1.GetOptions{})
 	require.Nil(t, err)
 
-	require.Nil(t, sensorv1alpha1.NodePhaseActive, sensor.Status.Phase)
+	require.Equal(t, sensorv1alpha1.NodePhaseActive, sensor.Status.Phase)
 
-	gwservice, err := test.K8sClient.CoreV1().Services(namespace).Get(fmt.Sprintf("%-svc", gatewayName), metav1.GetOptions{})
+	gwservice, err := test.K8sClient.CoreV1().Services(namespace).Get(fmt.Sprintf("%s-svc", gatewayName), metav1.GetOptions{})
 	require.Nil(t, err)
 
-	route, err := test.OCRouteClient.RouteV1().Routes(namespace).Create(&ocv1.Route{
+	route := &ocv1.Route{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: gwservice.Name,
 		},
 		Spec: ocv1.RouteSpec{
-			Host: "webhook-gateway-svc-argo-events.devkubewd.dev.blackrock.com",
+			Host: "webhook-gateway-svc-argo-events.",
 			To: ocv1.RouteTargetReference{
-				Kind:   "Service",
-				Name:   gwservice.Name,
-				Weight: 100,
+				Kind: "Service",
+				Name: gwservice.Name,
 			},
 			Port: &ocv1.RoutePort{TargetPort: intstr.FromString("example")},
 		},
-	})
+	}
+
+	routeBody, err := yaml.Marshal(route)
+	require.Nil(t, err)
+	require.NotNil(t, routeBody)
+
+	gvr := routeGVR()
+	mgvr := &metav1.GroupVersionResource{
+		Group:    gvr.Group,
+		Version:  gvr.Version,
+		Resource: gvr.Resource,
+	}
+
+	routeUObj, err := decodeAndUnstructure(routeBody, mgvr)
+	require.Nil(t, err)
+
+	routeClient := test.DynamicClient.Resource(routeGVR()).Namespace(namespace)
+
+	routeNewObj, err := routeClient.Create(routeUObj, metav1.CreateOptions{})
+	require.Nil(t, err)
+	require.NotNil(t, routeNewObj)
+
+	client := &http.Client{
+		Timeout: time.Second * 10,
+	}
+
+	reader := bytes.NewReader([]byte(`{"message":"this is my first webhook"}`))
+
+	response, err := client.Post("webhook-gateway-svc-argo-events./example", "application/json", reader)
+	require.Nil(t, err)
+	body, err := ioutil.ReadAll(response.Body)
+	require.Nil(t, err)
+	require.Equal(t, "success", string(body))
+
+	wfClient := test.DynamicClient.Resource(wfGVR()).Namespace(namespace)
+	wfs, err := wfClient.List(metav1.ListOptions{})
+	require.Nil(t, err)
+	require.Equal(t, 1, len(wfs.Items))
+
+	wfUObj := wfs.Items[0]
+	wf, err := encodeWorkflowFromUnstructured(&wfUObj)
+	require.Nil(t, err)
+
+	require.Equal(t, true, strings.Contains(wf.Name, "webhook-"))
+	require.NotEqual(t, "hello world", wf.Spec.Arguments.Parameters[0].Value)
+
+	err = routeClient.Delete(gwservice.Name, &metav1.DeleteOptions{})
+	require.Nil(t, err)
 }
